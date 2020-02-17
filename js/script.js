5eb9fa4e47f912e481109dabfd1e0d38414d2710
InfinityAxiom <59674338+InfinityAxiom@users.noreply.github.com>
2020-02-17 12:39:03 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,55 +2,37 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-
+   
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
-let studentList = document.getElementsByClassName('student-item')
-let numPerPage = 10
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-const showPage = (list, page) => {
-  startIndex = page * numPerPage - numPerPage;
-  endIndex = page * numPerPage;
-  //loops over everything
-  //want to pull the 10 that are on the "selected" page
-  //figure out how to call page / list correctly
-  for (i = startIndex; i < endIndex; i++) {
-    studentList[i].style.display = '';
-    if (i < startIndex || i > endIndex) {
-      studentList[i].style.display = 'none';
-    }
-  }
+
+let studentList = document.getElementsByClassName('student-item');
+let numPerPage = 10;
+
+const showPage = (list, page) =>{
+   startIndex = (page * numPerPage) - numPerPage;
+   endIndex = (page *numPerPage)
+//loops over everything
+//want to pull the 10 that are on the "selected" page
+//figure out how to call page / list correctly
+   for(i=0; i < studentList.length; i++) {
+      if(i >= startIndex && i < endIndex){
+         //make item visible on the page
+         studentList[i].style.display = 'block';
+      } else {
+         studentList[i].style.display = 'none';
+      }
+   }
 }
-//test showing/hiding items on the list
-showPage(0, 2);
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+showPage(0,6);
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
+
+
+
+
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
